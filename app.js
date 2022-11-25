d8505943c6e28292331a3b1e443aae7b165e1e94
lethanhkhoi <lethanhkhoi081001@gmail.com>
2022-11-25 10:48:31 +0700

    port 3000

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const routerCustom = require('./routes/index.js')
 const website = fs.readFileSync('view/index.html')
 const database = require('./utils/database')
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(cors())
