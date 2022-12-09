244a0ad2297b53229ef3f3de5ea45bf55a1df723
lethanhkhoi <lethanhkhoi081001@gmail.com>
2022-12-10 02:34:38 +0700

    chat

--- dataModel/chatCol.js
+++ dataModel/chatCol.js
@@ -3,6 +3,10 @@ const database = require('../utils/database')
 async function create(data) {
     return await database.chatModel().insertOne(data)
 }
+async function findByPresentationId(id) {
+    return await database.chatModel().findOne({ presentationId: id })
+}
 module.exports = {
     create,
+    findByPresentationId,
 }
--- socket/socket.js
+++ socket/socket.js
@@ -1,5 +1,9 @@
 const jwt = require('../utils/token')
 const database = require('../utils/database')
+const chatCol = require('../dataModel/chatCol')
+const messageCol = require('../dataModel/messageCol')
+const e = require('express')
+const ObjectID = require('mongodb').ObjectId
 
 const getUserInfo = async (token) => {
     const verify = await jwt.verifyToken(token)
@@ -12,11 +16,32 @@ const getUserInfo = async (token) => {
     return user[0]
 }
 module.exports = (socket) => {
-    socket.on('chatInit', async (data) => {
+    socket.on('present', async (data) => {
         const token = socket.handshake.headers.token
         const user = await getUserInfo(token)
-        console.log('token ne ', token)
-        console.log('data ne ', user)
+        const chat = await chatCol.findByPresentationId(data.presentationId)
+        socket.on(`present-${data.presentationId}`, (data) => {
+            socket.broadcast.emit(`present-${data.presentationId}`, data)
+        })
+        socket.on(`chat-${chat.id}`, async (data) => {
+            const newMessage = {
+                id: ObjectID().toString(),
+                userId: user.id,
+                message: data.message,
+                chatId: chat.id,
+            }
+            const result = await messageCol.create(newMessage)
+            if (!result) {
+                socket.broadcast.emit(`chat-${chat.id}`, {
+                    errorCode: true,
+                    data: 'System error',
+                })
+            }
+            socket.broadcast.emit(`chat-${chat.id}`, {
+                errorCode: null,
+                data: newMessage,
+            })
+        })
     })
     socket.on('disconnect', () => {
         console.log('user disconnected')
