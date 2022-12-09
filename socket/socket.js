a999eee026a4d76bd25c25de3f35ff0da4baba30
lethanhkhoi <lethanhkhoi081001@gmail.com>
2022-12-09 14:41:18 +0700

    authentication socket

--- socket/socket.js
+++ socket/socket.js
@@ -1,8 +1,22 @@
+const jwt = require('../utils/token')
+const database = require('../utils/database')
+
+const getUserInfo = async (token) => {
+    const verify = await jwt.verifyToken(token)
+    let user = []
+    user = await database.userModel().find({ email: verify?.email }).toArray()
+
+    if (user.length == 0 || user.length > 1) {
+        return null
+    }
+    return user[0]
+}
 module.exports = (socket) => {
-    socket.on('chatInit', (data) => {
+    socket.on('chatInit', async (data) => {
         const token = socket.handshake.headers.token
+        const user = await getUserInfo(token)
         console.log('token ne ', token)
-        console.log('data ne ', data)
+        console.log('data ne ', user)
     })
     socket.on('disconnect', () => {
         console.log('user disconnected')
