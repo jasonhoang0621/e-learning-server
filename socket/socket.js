869a978410efb7196b8f2f63ea4721ff408aa9f9
lethanhkhoi <lethanhkhoi081001@gmail.com>
2022-12-09 14:04:23 +0700

    set up socket

--- socket/socket.js
+++ socket/socket.js
@@ -1,4 +1,9 @@
 module.exports = (socket) => {
+    socket.on('chatInit', (data) => {
+        const token = socket.handshake.headers.token
+        console.log('token ne ', token)
+        console.log('data ne ', data)
+    })
     socket.on('disconnect', () => {
         console.log('user disconnected')
     })
